3d7a58972dd7f06e708f6975b71c60cbe564f61a
Saumy003 <saumysingh2005@gmail.com>
2025-10-21 18:08:26 +0530

    array

---
+++ a_rray.py
@@ -0,0 +1,12 @@
+from array import *
+a1 = array('i', [23,56,11])
+
+print(a1)                    # array('i', [23, 56, 11])
+
+for x in a1:
+    print(x)                 # 23 56 11
+
+for i in range(3):
+    print(a1[i] , end=' ')   # 23 56 11
+
+# array methods.

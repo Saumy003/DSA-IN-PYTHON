83e4de5b95dd632e4b9311d85efd89f2d35a9d45
Saumy003 <saumysingh2005@gmail.com>
2026-03-22 11:40:44 +0530

    basic code, count the digit, armstrong num, freq map,hashing

--- digit_extraction.py/hashing.py
+++ digit_extraction.py/hashing.py
@@ -1,7 +1,8 @@
 # Hashing in python
 
-# Number hashing 
-#Approach - 1 => Brute Force
+# Number hashing =>
+
+# Approach - 1 => Brute Force
 
 p = [5, 3, 2, 2, 1, 5, 5, 7, 5, 10]
 q = [10, 111, 1, 9, 5, 67, 2]
@@ -28,3 +29,22 @@ for num in m:
     else:
         print(hash_list[num])
 
+# Approach 3 => Using Dictionaries Instead
+
+
+# Character hashing =>
+
+s = "azyxyyzaaaa"
+r = ["d", "a", "y", "x"]
+
+freq_list = [0] * 26
+
+for ch in s:
+    ascii_value = ord(ch)
+    index = ascii_value - 97
+    freq_list[index] += 1
+
+for ch in r:
+    ascii_value = ord(ch)
+    index = ascii_value - 97
+    print(freq_list[index])

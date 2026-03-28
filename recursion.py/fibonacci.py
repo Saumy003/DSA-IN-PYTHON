6a0fe19bfd753d90cc72a7479c73fb1b5b3a7de5
Saumy003 <saumysingh2005@gmail.com>
2026-03-28 09:56:15 +0530

    recursion concept, functional recursion, reverse array,check palindrome, fibonacci number

--- recursion.py/fibonacci.py
+++ recursion.py/fibonacci.py
@@ -1,1 +1,10 @@
 # Find the fibonacci number #
+# Using recursion =>
+
+def func(n):
+    if n == 0 or n == 1:
+        return n
+    return func(n-1) + func(n-2)
+    
+fibonacci_number = func(int(input("Enter a number:")))
+print("Fibonacci Number is:" , fibonacci_number)

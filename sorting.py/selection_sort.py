d39fadc68e40210494326256ecfedd5f5b8c7a86
Saumy003 <saumysingh2005@gmail.com>
2026-03-28 18:37:25 +0530

    Selection-sort

--- sorting.py/selection_sort.py
+++ sorting.py/selection_sort.py
@@ -1,1 +1,16 @@
 # SELECTION SORT #
+
+nums = [ 5, 7, 8, 4, 1, 6, 9, 2]
+
+def selection_sort(nums):
+    n = len(nums)
+    for i in range(0 , n):
+        mini_index = i
+        for j in range(i+1 , n):
+            if nums[j] < nums[mini_index]:
+                mini_index = j
+
+        nums[i] , nums[mini_index] = nums[mini_index] , nums[i]
+
+selection_sort(nums)
+print("Sorted List is:", nums)

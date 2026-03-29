2c22f3a4ee17391fde032aa70db2503fccd36eb8
Saumy003 <saumysingh2005@gmail.com>
2026-03-29 15:28:30 +0530

    Selection-sort, Bubble-sort, Insertion-sort, Merge-sort

--- sorting.py/merge_sort.py
+++ sorting.py/merge_sort.py
@@ -1,1 +1,42 @@
 # MERGE SORT #
+
+# Given array =>
+nums = [3, 1, 2, 4, 1, 5, 2, 6, 4]
+
+# Code to divide array in two part recursivly and sort it =>
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums       # base case of this recursive function
+    
+    mid = len(nums) // 2
+    left_arr = nums[: mid]
+    right_arr = nums[mid :]
+    left = merge_sort(left_arr)
+    right = merge_sort(right_arr)
+    return merge_array(left, right)
+
+# Code to merge two sorted are =>
+def merge_array(left, right):
+    result = []
+    i, j = 0, 0
+    n, m = len(left), len(right)
+    while i < n and j < m:
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    if i < n:
+        while i< n:
+            result.append(left[i])
+            i += 1
+    if j < m:
+        while j< m:
+            result.append(right[j])
+            j += 1
+    return result
+
+print("Sorted Array is:",merge_sort(nums))   #result = [1, 1, 2, 2, 3, 4, 4, 5, 6]
+        

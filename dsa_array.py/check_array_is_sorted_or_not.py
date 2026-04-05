b488a047505730bb50552b6fd8073a3f1fca9c9e
Saumy003 <saumysingh2005@gmail.com>
2026-04-05 23:38:26 +0530

    check is the array is sorted or not

--- dsa_array.py/check_array_is_sorted_or_not.py
+++ dsa_array.py/check_array_is_sorted_or_not.py
@@ -1,5 +1,14 @@
-""" Check if array is sorted or not =>
+""" Check is given array is sorted or not =>
 if sorted return True
 if nt sorted return False
 """
 
+nums = [3, 5, 6, 8, 9, 10, 20]
+n = len(nums)
+
+is_sorted = True
+for i in range(0, n-1):
+    if nums[i] > nums[i+1]:
+        is_sorted = False
+
+print(is_sorted)

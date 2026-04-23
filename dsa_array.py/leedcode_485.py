c8ee2d98aaef609ebc91540fc6b46a6f83002d0b
Saumy003 <saumysingh2005@gmail.com>
2026-04-23 12:44:40 +0530

    leetcode- 485 i.e. Max Consecutive Ones

---
+++ dsa_array.py/leedcode_485.py
@@ -0,0 +1,20 @@
+"""
+Leetcode - 485
+Max Consecutive Ones
+"""
+
+nums = [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1]
+
+n = len(nums)
+count = 0
+max_count = 0
+
+for i in range(0, n):
+    if nums[i] == 1:
+        count += 1
+
+    else:
+        max_count = max(max_count , count)
+        count = 0
+
+print("Maximun consecutive ones is:" , max(max_count , count))

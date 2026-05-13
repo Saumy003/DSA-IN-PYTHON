9b7bdb1c85971ce36a1375ed7bc71e79de440c35
Saumy003 <saumysingh2005@gmail.com>
2026-05-13 15:55:53 +0530

    Lower bound Implementation

---
+++ binary_search/lower_bound.py
@@ -0,0 +1,22 @@
+"""
+Lower Bound Implementetion
+"""
+
+arr = [2, 3, 7, 10, 11, 11, 25]
+target = 9
+
+lower_bound = len(arr)       
+low = 0
+high = len(arr) - 1
+
+while low<=high:
+
+    mid = (low + high) // 2
+
+    if arr[mid] >= target:            #key concept
+        lower_bound = mid
+        high = mid - 1
+    
+    else:
+        low = mid + 1
+print("Lower Bound:", lower_bound)

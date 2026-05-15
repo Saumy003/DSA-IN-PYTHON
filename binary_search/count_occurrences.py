07b146c1158733cd1e6e86664fcece8e895c46c5
Saumy003 <saumysingh2005@gmail.com>
2026-05-15 19:49:16 +0530

    Question: Count occurrences of a number in a sorted array with duplicates.

---
+++ binary_search/count_occurrences.py
@@ -0,0 +1,54 @@
+"""
+Question: Count occurrences of a number in a sorted array with duplicates.
+"""
+# Brute Force
+
+nums = [1, 2, 3, 3, 3, 3, 3, 5, 6, 8, 9, 9, 10]
+target = 3
+first = -1
+last = -1
+
+for i in range(0, len(nums)):
+    if nums[i] == target:
+        if first == -1:
+            first = i
+        last = i
+
+print("number of occurrance is:", last-first+1)
+
+# Optimal solution
+
+nums = [1, 2, 3, 3, 3, 3, 3, 5, 8, 9, 9, 10, 10, 10]
+target = 10
+
+# first occurrance is associated with lower bound
+lower_bound = -1
+
+low = 0
+high = len(nums) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+    
+    if nums[mid] >= target:
+        lower_bound = mid
+        high = mid - 1
+
+    else:
+        low = mid +1
+# last occurrance is associated with upper bound
+upper_bound = len(nums)
+
+low = 0
+high = len(nums) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if nums[mid] > target:
+        upper_bound = mid
+        high = mid - 1
+    else:
+        low = mid +1
+
+print("Number of occurance is :", upper_bound - lower_bound)

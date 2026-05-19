dec15a197ceee7796ead28858483cacec5829b72
Saumy003 <saumysingh2005@gmail.com>
2026-05-19 22:35:02 +0530

    2. Traversal in Singly Linked List

--- linked_list/leetcode_876.py
+++ linked_list/leetcode_876.py
@@ -1,14 +1,30 @@
-""" 2. Traversal in Singly Linked List """
+""" L -> 867"""
 
 class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
-
-
+        
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
+
+
+    def append(self, val):
+        new_node = Node(val)
+
+        if self.head is None:
+            self.head = new_node
+        
+        else:
+            curr = self.head
+
+            while curr.next is not None:
+                curr = curr.next
+
+            curr.next = new_node
+
+
 
     def traversal(self):
         if self.head is None:
@@ -21,15 +37,11 @@ class SinglyLinkedList:
                 curr = curr.next
 
 
-# object creation
+
 l1 = SinglyLinkedList()
 
-
-"""append values
-ll.append(20)
-ll.append(30)
+l1.append(20)
+l1.append(30)
 l1.append(40)
-"""
 
-#traversal
-l1.traversal()           #20 30 40
+l1.traversal()

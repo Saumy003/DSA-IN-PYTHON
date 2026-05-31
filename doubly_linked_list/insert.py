97137f56b84cef8f9d8e9226c9372d57ece03314
Saumy003 <saumysingh2005@gmail.com>
2026-05-31 12:45:54 +0530

    2. Insert at Head in DLL

--- doubly_linked_list/insert.py
+++ doubly_linked_list/insert.py
@@ -1,1 +1,28 @@
-""" 2. Insert """
+""" 2. Insert  at Head """ 
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert at Head
+    def insert_at_head(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+
+dll = DoublyLinkedList()
+dll.insert_at_head(10)
+dll.insert_at_head(20)

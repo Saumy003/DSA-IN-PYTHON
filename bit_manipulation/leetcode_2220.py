20ddb39c5cb8fe40820fafa4f046a7ba7424a187
Saumy003 <saumysingh2005@gmail.com>
2026-06-05 12:25:37 +0530

    Minimum Bit Flips to Convert Number

--- bit_manipulation/leetcode_2220.py
+++ bit_manipulation/leetcode_2220.py
@@ -1,3 +1,21 @@
 """
-Leetcodde : 2220
+Leetcode: 2220  --> Minimum Bit Flips to Convert Number
 """
+
+def count_flips():
+    start = 3
+    goal = 4
+    count = 0
+
+    #applying xor operator
+    xor = start ^ goal
+
+    #count the number of ones in xor
+    for i in range (0, 32):
+        if xor & (1 << i) != 0:
+            count += 1
+
+
+    print("Minimum Bit Flips:", count)
+
+count_flips()

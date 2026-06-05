108bdf80b54c904926faf04f63ada6ba411c7e91
Saumy003 <saumysingh2005@gmail.com>
2026-06-05 14:07:49 +0530

    Leetcode: 78 --> Genetate Subsets Using Bit Manipulation.

--- bit_manipulation/leetcode_78.py
+++ bit_manipulation/leetcode_78.py
@@ -1,3 +1,20 @@
 """
-Leetcode: 78
+Leetcode: 78 --> Genetate Subsets Using Bit Manipulation.
 """
+
+nums = [1, 2, 3]
+
+n = len(nums)
+total_subsets = 1 << n
+result = []
+
+for num in range(0, total_subsets):
+    lst = []
+
+    for i in range(0, n):
+        if num & (1 << i) != 0:
+            lst.append(nums[i])
+        
+    result.append(lst)
+
+print(result)

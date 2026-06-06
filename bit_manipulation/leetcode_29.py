7c33d63844997afc445f77c719879580a0e32eb1
Saumy003 <saumysingh2005@gmail.com>
2026-06-06 19:46:02 +0530

    Leetcode:29 --> Divide Two Integers

--- bit_manipulation/leetcode_29.py
+++ bit_manipulation/leetcode_29.py
@@ -1,3 +1,30 @@
-""" LC : 29
-Divide Two Integers
 """
+Leetcode:29 --> Divide Two Integers
+"""
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        quotient = 0
+
+        while dividend >= divisor:
+
+            temp = divisor
+            multiple = 1
+
+            while dividend >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+
+            dividend -= temp
+            quotient += multiple
+
+        return sign * quotient

b6ed9ae4910d20c2f4bb3de39f4043385ad42235
Saumy003 <saumysingh2005@gmail.com>
2026-06-30 23:00:07 +0530

    Combination Sum - 1

--- adv_recursion/7. Leetcode_39.py
+++ adv_recursion/7. Leetcode_39.py
@@ -1,2 +1,33 @@
 """ Combination Sum """
 
+nums = [2,3,6,7]
+result = []
+target = 7
+
+def combinationSum(index, total, subset):
+
+    # base case
+    if total == target:
+        result.append(subset.copy())
+        return
+
+    elif total > target:
+        return
+
+    if index >= len(nums):
+        return
+
+    # pick
+    Sum = total + nums[index]
+    subset.append(nums[index])
+    combinationSum(index, Sum, subset)
+
+    # backtrack
+    subset.pop()
+
+    # not pick
+    combinationSum(index + 1, total, subset)
+
+
+combinationSum(0,0,[])
+print(result)

ae11880f0e416b893855507374ad37cbb62d7c80
Saumy003 <saumysingh2005@gmail.com>
2026-08-07 17:13:52 +0530

    LC:17 => Letter Combinatons of a Phone Number

--- adv_recursion/11.Leetcode_17.py
+++ adv_recursion/11.Leetcode_17.py
@@ -1,4 +1,4 @@
-""" LC -> 17 Letter Combinatons of a Phone Number """
+""" Letter Combinatons of a Phone Number """
 
 digits = "23"
 char_map = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}

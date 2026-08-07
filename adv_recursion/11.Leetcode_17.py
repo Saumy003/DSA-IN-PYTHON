b4c50cc396f1d081a0adeef502fa13c6c88cfa6f
Saumy003 <saumysingh2005@gmail.com>
2026-08-07 17:05:31 +0530

    Letter Combinatons of a Phone Number

---
+++ adv_recursion/11.Leetcode_17.py
@@ -0,0 +1,19 @@
+""" LC -> 17 Letter Combinatons of a Phone Number """
+
+digits = "23"
+char_map = {"2" : "abc", "3" : "def", "4" : "ghi", "5" : "jkl", "6" : "mno", "7" : "pqrs", "8" : "tuv", "9" : "wxyz"}
+result = []
+
+def phoneNumber(index, subset):
+
+    #base case
+    if index >= len(digits):
+        result.append("".join(subset))
+        return
+    for ch in char_map[digits[index]]:
+        subset.append(ch)
+        phoneNumber(index+ 1, subset)
+        subset.pop()
+
+phoneNumber(0, [])
+print(result)

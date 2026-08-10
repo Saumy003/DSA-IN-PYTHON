6333827e69010b632aa18725b05ea45571886612
Saumy003 <saumysingh2005@gmail.com>
2026-08-10 23:58:05 +0530

    N Queen Problem of Leetcode

--- adv_recursion/12.Leetcode_51.py
+++ adv_recursion/12.Leetcode_51.py
@@ -60,3 +60,39 @@ print(ans)
 
 # Optimal Solution
 
+n = 4
+ans = []
+board = ["." * n for _ in range(n)]
+leftrow = [0] * n
+upperDiagonal = [0] * (2 * n - 1)
+lowerDiagonal = [0] * (2 * n - 1)
+
+def solve(col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
+    if col == n:
+        ans.append(board[:])
+        return
+
+    for row in range(n):
+        if (
+            leftrow[row] == 0
+            and lowerDiagonal[row + col] == 0
+            and upperDiagonal[n - 1 + col - row] == 0
+        ):
+            board[row] = board[row][:col] + "Q" + board[row][col + 1 :]
+            leftrow[row] = 1
+            lowerDiagonal[row + col] = 1
+            upperDiagonal[n - 1 + col - row] = 1
+
+            solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+
+            board[row] = board[row][:col] + "." + board[row][col + 1 :]
+            leftrow[row] = 0
+            lowerDiagonal[row + col] = 0
+            upperDiagonal[n - 1 + col - row] = 0
+
+    return ans
+
+solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+print(ans)
+
+# Output => [['..Q.', 'Q...', '...Q', '.Q..'], ['.Q..', '...Q', 'Q...', '..Q.']]

c4b502176fb8d837bb5258b1749afa423db368ce
Saumy003 <saumysingh2005@gmail.com>
2026-08-31 17:32:40 +0530

    LC -> 20. Valid Parentheses

---
+++ stacks_&_queues/8. Valid_Parentheses.py
@@ -0,0 +1,26 @@
+""" Topic 8. Valid Parentheses --> LC => 20 """
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for bracket in s:
+            # If opening bracket, push to stack
+            if bracket in "([{":
+                stack.append(bracket)
+            else:
+                # For closing bracket, stack cannot be empty
+                if len(stack) == 0:
+                    return False
+                
+                # Pop last opening bracket and check if matching
+                ch = stack.pop()
+                if (bracket == ")" and ch == "(") or \
+                   (bracket == "]" and ch == "[") or \
+                   (bracket == "}" and ch == "{"):
+                    continue
+                else:
+                    return False
+        
+        # Check if all opened brackets were closed
+        return len(stack) == 0
